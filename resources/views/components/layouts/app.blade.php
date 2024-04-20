85bcf2047f0c5b9dc294df41bb1269efe1fe7511
K <JuanEscobarGT@outlook.com>
2024-04-20 00:00:45 -0600

    [lib] Instalación de MaryUI (falta configuración)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -39,22 +39,7 @@
     </x-mary-nav>
 
     {{-- The main content with `full-width` --}}
-    <x-mary-main with-nav full-width>
-
-        {{-- This is a sidebar that works also as a drawer on small screens --}}
-        {{-- Notice the `main-drawer` reference here --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-200">
-
-            {{-- Activates the menu item when a route matches the `link` property --}}
-            <x-mary-menu activate-by-route active-bg-color="bg-sky-500/20">
-                <x-mary-menu-item title="Home" icon="o-home" link="###" />
-                <x-mary-menu-item title="Messages" icon="o-envelope" link="###" />
-                <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
-                    <x-mary-menu-item title="Wifi" icon="o-wifi" link="####" />
-                    <x-mary-menu-item title="Archives" icon="o-archive-box" link="{{ route('dashboard') }}" />
-                </x-mary-menu-sub>
-            </x-mary-menu>
-        </x-slot:sidebar>
+    <x-mary-main full-width>
 
         {{-- The `$slot` goes here --}}
         <x-slot:content>
@@ -65,5 +50,6 @@
     {{--  TOAST area --}}
     <x-mary-toast />
 </body>
+
 
 </html>
